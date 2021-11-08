6da57e8278f2e82a6f42ad0b229066fe670d7a8e
StrangeParadox <d.ellis01.dje@gmail.com>
2021-11-08 02:43:44 +0100

    ✌️

--- bash/fetch/fetch.sh
+++ bash/fetch/fetch.sh
@@ -1,3 +1,8 @@
 OS_NAME=`/bin/cat /etc/os-release | grep PRETTY_NAME | sed 's/\"//g' | sed 's/.*=//g'`
+USER_NAME=$(whoami)
+PACKAGES=`pacman -Qq | wc -l`
 
-echo $OS_NAME
+
+echo -e "Operating System:  " $OS_NAME
+echo -e "Username:          " $USER_NAME
+echo -e "Packages:          " $PACKAGES
