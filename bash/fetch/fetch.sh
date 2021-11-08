3c3b9488984e3cfdde5471f8aad83f26e89f5b2f
StrangeParadox <d.ellis01.dje@gmail.com>
2021-11-08 02:44:58 +0100

    removed space

--- bash/fetch/fetch.sh
+++ bash/fetch/fetch.sh
@@ -2,7 +2,6 @@ OS_NAME=`/bin/cat /etc/os-release | grep PRETTY_NAME | sed 's/\"//g' | sed 's/.*
 USER_NAME=$(whoami)
 PACKAGES=`pacman -Qq | wc -l`
 
-
 echo -e "Operating System:  " $OS_NAME
 echo -e "Username:          " $USER_NAME
 echo -e "Packages:          " $PACKAGES
